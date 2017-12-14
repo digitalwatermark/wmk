7edabf9c2f5e940379e366155a45039b0fb0b46e
wangyini666 <beautyking21@sina.com>
2017-12-14 17:36:00 +0800

    add_path

--- wmk_add_ext/add_path.m
+++ wmk_add_ext/add_path.m
@@ -3,21 +3,21 @@
 
 home_path = cd;
 addpath(home_path)
-third_party_path = [home_path,'\third_party_tools'];
+% third_party_path = [home_path,'\third_party_tools'];
 
 
  addpath(genpath([home_path,'\subroutines']))
- addpath(genpath([home_path,'\doc']))
+%  addpath(genpath([home_path,'\doc']))
 
 
 
 % third party tools
-addpath(genpath([third_party_path,'\EAQUAL']))
-addpath(genpath([third_party_path,'\FAAC']))
-addpath(genpath([third_party_path,'\NeroAAC']))
-addpath(genpath([third_party_path,'\LAME']))
-addpath(genpath([third_party_path,'\MPG123']))
-addpath(genpath([third_party_path,'\TSM']))
+% addpath(genpath([third_party_path,'\EAQUAL']))
+% addpath(genpath([third_party_path,'\FAAC']))
+% addpath(genpath([third_party_path,'\NeroAAC']))
+% addpath(genpath([third_party_path,'\LAME']))
+% addpath(genpath([third_party_path,'\MPG123']))
+% addpath(genpath([third_party_path,'\TSM']))
 
 
 
