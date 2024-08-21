c36dd658017d1adcc16450e8474e9b857177f0ae
wangyini666 <33109128+wangyini666@users.noreply.github.com>
2024-08-21 10:01:23 +0800

    Delete WMAnalogWindowQt_doubleCCTV_v6 directory

--- WMAnalogWindowQt_doubleCCTV_v6/include/Analogfunction.h
+++
@@ -1,7 +0,0 @@
-#include "Analogdefine.h"
-
-void AnalogInit();
-
-void AnalogData_swich(int BoxNum);
-
-DWORD CALLBACK waveInProc(HWAVEIN hwi, UINT uMsg, DWORD dwInstance, DWORD dwParam1, DWORD dwParam2);
